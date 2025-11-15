54969e928175e64c534aa082abaa8c92c1dcb79c
Aldalilixz <rinrada_lady27@outlook.com>
2025-11-16 00:18:40 +0700

    feat: make buttomsheet word-addable, make card clickable, loop the word when clicking card on test for adding word

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/state_manager.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,28 +34,54 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var wordController = TextEditingController();
+  double screenWidth = 0;
+  List<String> words = ['何'];
+  Rx<int> index = 0.obs;
+  String word = '';
+  void _addWord() {
+    word = wordController.value.text;
+    words.add(word);
+    Navigator.pop(context);
+  }
 
-void _addWord() {
-  showModalBottomSheet<void>(
-            context: context,
-            builder: (BuildContext context) {
-              return const SizedBox(
-                height: 400,
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text('Add words here'),
-                    ],
-                  ),
+  void _addWordBottomSheet() {
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return SizedBox(
+          height: 400,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                    width: screenWidth,
+                    alignment: FractionalOffset.topRight,
+                    child: IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: const Icon(Icons.clear))),
+                const Text('Add words here'),
+                TextField(
+                    keyboardType: TextInputType.text,
+                    controller: wordController),
+                ElevatedButton(
+                  onPressed: _addWord,
+                  child: const Text('OK'),
                 ),
-              );
-            },
-          );
-}
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Container(
         color: Colors.red,
@@ -59,14 +89,30 @@ void _addWord() {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Card(
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
-                child: Container(
-                  padding: const EdgeInsets.symmetric(vertical: 60, horizontal: 150),
-                  child: const Text(
-                  '何',
-                  style: TextStyle(color: Colors.amber),
-                              ),
+              GestureDetector(
+                onTap: () {
+                  if (index == words.length - 1) {
+                    index.value = 0;
+                  } else {
+                    index.value += 1;
+                  }
+                },
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0)),
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: screenWidth * 0.8,
+                    height: 150,
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 30, horizontal: 30),
+                    child: Obx(
+                      () => Text(
+                        words[index.value],
+                        style: TextStyle(color: Colors.amber),
+                      ),
+                    ),
+                  ),
                 ),
               )
             ],
@@ -74,7 +120,7 @@ void _addWord() {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _addWord,
+        onPressed: _addWordBottomSheet,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,10 +3,32 @@ import 'package:flutter_flash_card/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('should found 1 card in the Home screen', (WidgetTester tester) async {
+  testWidgets('should found 1 card with word and add button in the Home screen',
+      (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
 
     expect(find.text('何'), findsOneWidget);
     expect(find.byType(Card), findsOneWidget);
+    expect(find.byType(FloatingActionButton), findsOneWidget);
+  });
+
+  testWidgets(
+      'should show bottom sheet that can add a word and auto close after word added',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+    await tester.tap(find.byType(FloatingActionButton));
+    await tester.pumpAndSettle();
+
+    expect(find.byType(BottomSheet), findsOneWidget);
+    expect(find.text('Add words here'), findsOneWidget);
+    expect(find.byType(TextField), findsOneWidget);
+    expect(find.byType(ElevatedButton), findsOneWidget);
+    expect(find.text('OK'), findsOneWidget);
+
+    await tester.enterText(find.byType(TextField), 'pop');
+    await tester.tap(find.text('OK'));
+
+    await tester.pumpAndSettle();
+    expect(find.byType(BottomSheet), findsNothing);
   });
 }
